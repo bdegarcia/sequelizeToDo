b67b288e26d1ed882a922df49876b5819c477616
Ben de Garcia <degarcia.ben@gmail.com>
2020-08-06 18:48:16 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+#Sequelizze ToDo
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const db = require("./models")
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Starting our Express app
+// =============================================================
+
+
+db.sequelize.sync().then(()=> {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+})
