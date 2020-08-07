5d6e51357be8d5b3e67d5646319d938249263253
Ben de Garcia <degarcia.ben@gmail.com>
2020-08-06 20:49:03 -0500

    added delet and put routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,26 +10,31 @@ module.exports = function(app) {
   // GET route for getting all of the todos
   app.get("/api/todos", function(req, res) {
     db.Todo.findAll()
-    .then((data) => {
-      res.json(data)
-    })
+    .then((data) => {res.json(data)})
+    .catch(err => {throw err})
   });
 
   // POST route for saving a new todo. We can create a todo using the data on req.body
   app.post("/api/todos", function(req, res) {
-    db.Todo.create(req.body).then((data) => {
-      res.json(data);
-    })
+    db.Todo.create(req.body)
+    .then((data) => {res.json(data)})
+    .catch(err => {throw err})
   });
 
   // DELETE route for deleting todos. We can access the ID of the todo to delete in
   // req.params.id
   app.delete("/api/todos/:id", function(req, res) {
-    res.end()
+    const id = req.params.id;
+    db.Todo.destroy({where: { id }})
+    .then((data) => { res.json(data)})
+    .catch(err => {throw err})
   });
 
   // PUT route for updating todos. We can access the updated todo in req.body
   app.put("/api/todos", function(req, res) {
-    res.end()
+    const {id, text, complete} = req.body;
+    db.Todo.update({ text, complete }, {where: { id }})
+    .then((data) => { res.json(data)})
+    .catch(err => {throw err})
   });
 };
